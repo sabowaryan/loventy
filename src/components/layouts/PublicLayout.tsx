ffdd84acf68380a6bb0cd97c30fd66da665b9439
Ryan SABOWA <screentapinc@gmail.com>
2025-06-27 14:44:08 +0100

    Update PublicLayout.tsx number phone

--- src/components/layouts/PublicLayout.tsx
+++ src/components/layouts/PublicLayout.tsx
@@ -357,7 +357,7 @@ const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
                   <svg className="h-4 w-4 text-[#D4A5A5]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <a href="tel:+243812345678" className="hover:text-[#D4A5A5] transition-colors">+243 81 234 5678</a>
+                  <a href="tel:+243981682933" className="hover:text-[#D4A5A5] transition-colors">+243 98 168 2933</a>
                 </li>
                 <li className="flex items-start space-x-2">
                   <svg className="h-4 w-4 text-[#D4A5A5] mt-1 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">

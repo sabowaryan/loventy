75d4d4f88af4845a88902dc74f096ec55838b6b8
Ryan SABOWA <screentapinc@gmail.com>
2025-06-29 19:16:34 +0100

    Updated Editor.tsx

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -274,9 +274,9 @@ const Editor: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-[#FAF9F7]">
+    <div className="min-h-screen bg-[#FAF9F7] pt-16"> {/* Added pt-16 for fixed header */}
       {/* Fixed Header */}
-      <div className="sticky top-0 z-40 bg-white border-b border-gray-200 shadow-sm">
+      <div className="fixed top-0 left-0 right-0 z-40 bg-white border-b border-gray-200 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Left Navigation */}
